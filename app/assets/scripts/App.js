f666f4a43236b3836d2e4465f6cb9b8b835f0316
mihai <cos_mihai23@yahoo.com>
2018-09-01 14:09:54 +0200

    webpack integrated, trying to integrate babel

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,5 @@
+const Person = require('./modules/test');
+
+const mihai = new Person('mihai', 'blue');
+
+console.log(mihai.color)
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+    filename: "App.js"
+  },
+  // babel integration
+  // module: {
+  //   rules: [
+  //     {
+  //       loader: 'babel-loader',
+  //       query: {
+  //         presets: ['es2015']
+  //       },
+  //       test: /\.js$/,
+  //       exclude: /node_modules/
+  //     }
+  //   ]
+  // }
+}
