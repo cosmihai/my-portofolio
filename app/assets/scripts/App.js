c9a731c073eb7e1b20d5c22be222788083a24891
mihai <cos_mihai23@yahoo.com>
2018-09-06 23:35:34 +0200

    sticky header and smooth scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu =  new MobileMenu();
 new RevealOnScroll($(".card"), "70%");
+const stickyHeader = new StickyHeader();
