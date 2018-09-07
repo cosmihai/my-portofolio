dd7a2507eb47c69a75376b21e2c063dc2ce0a396
mihai <cos_mihai23@yahoo.com>
2018-09-07 15:47:07 +0200

    modal added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,9 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 const mobileMenu =  new MobileMenu();
 new RevealOnScroll($(".card"), "70%");
 const stickyHeader = new StickyHeader();
+const modal = new Modal();
