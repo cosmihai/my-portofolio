7d1ee06881f3d3592915ccf6fcb417b4ed54f684
cosmihai <cos_mihai23@yahoo.com>
2021-01-04 09:02:29 +0200

    del babel, gulp and start modifie webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,23 @@ const path = require('path');
 module.exports = {
   entry: "./app/assets/scripts/App.js",
   output: {
-    path: path.resolve(__dirname, "./app/temp/scripts"),
-    filename: "App.js"
+    path: path.resolve(__dirname, "app"),
+    filename: "bundled.js"
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      }
-    ]
-  }
+  mode: "development",
+  watch: true
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['env']
+  //         }
+  //       }
+  //     }
+  //   ]
+  // }
 }
